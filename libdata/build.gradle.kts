beedaeade929cad748ed9b193c5a7c7d262d02bd
Kevin Bourrillion <kevinb9n@gmail.com>
2022-11-13 07:56:58 -0800

    update dependencies

--- libdata/build.gradle.kts
+++ libdata/build.gradle.kts
@@ -7,10 +7,7 @@
  */
 
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.6.21"
-
-    // Apply the java-library plugin for API and implementation separation.
+    id("org.jetbrains.kotlin.jvm") version "1.7.20"
     `java-library`
 }
 
@@ -20,21 +17,11 @@ repositories {
 }
 
 dependencies {
-    // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:31.0.1-jre")
+    api("com.google.guava:guava:31.1-jre")
 
-    // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-    // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
 }
