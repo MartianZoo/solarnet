fbc1bf237da039e9fb9d6c602e9d0d7e89d30103
Kevin Bourrillion <kevinb9n@gmail.com>
2022-11-19 14:11:02 -0800

    reuse some gradle stuff

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+
+subprojects {
+  repositories {
+    mavenCentral()
+  }
+
+  tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+      exceptionFormat = FULL
+      showExceptions = true
+      showStackTraces = true
+    }
+  }
+}
--- libdata/build.gradle.kts
+++ libdata/build.gradle.kts
@@ -1,32 +1,18 @@
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.7.20"
-    `java-library` // what does this do?
-}
-
-repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
 }
 
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    implementation("com.google.guava:guava:31.1-jre")
     implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    testImplementation("com.google.truth:truth:1.0")
 }
 
-tasks.withType<Test> {
-  useJUnitPlatform()
-  testLogging {
-    exceptionFormat = FULL
-    showExceptions = true
-    showStackTraces = true
-  }
+dependencies {
+  implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+  implementation("com.google.guava:guava:31.1-jre")
+
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+  testImplementation("com.google.truth:truth:1.0")
 }
