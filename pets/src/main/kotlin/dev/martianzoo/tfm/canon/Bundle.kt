eac0cfef5ca88e256528a95ae9f07a2b156d67fd
kevinb9n <kevinb9n@gmail.com>
2022-12-22 14:29:13 -0800

    rearrange components in components.pets, section headings and stuff

--- pets/src/main/kotlin/dev/martianzoo/tfm/canon/Bundle.kt
+++ pets/src/main/kotlin/dev/martianzoo/tfm/canon/Bundle.kt
@@ -2,8 +2,8 @@ package dev.martianzoo.tfm.canon
 
 enum class Bundle(val id: Char) {
   BASE('B'),
-  CORPORATE_ERA('R'), // well it appears 3 times...
-  THARSIS('M'), // for "map", ooh
+  CORPORATE_ERA('R'),  // well the letter R appears 3 times so...
+  THARSIS('M'),        // for "map", ooh
   HELLAS('H'),
   ELYSIUM('E'),
   VENUS_NEXT('V'),
