1d1c733bca955ddd13a73036d8d4b66f75f68788
kevinb9n <kevinb9n@gmail.com>
2022-12-28 11:53:32 -0800

    Update dependencies - IDEA+gradle make it trivially easy!

--- pets/build.gradle.kts
+++ pets/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     implementation("com.github.h0tk3y.betterParse:better-parse:0.4.4")
-    implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
 }
 
 dependencies {
@@ -13,9 +13,9 @@ dependencies {
 
   implementation("com.google.guava:guava:31.1-jre")
 
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-  testImplementation("com.google.truth:truth:1.0")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+  testImplementation("com.google.truth:truth:1.1.3")
 }
 
 sourceSets {
