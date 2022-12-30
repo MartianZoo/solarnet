c5be790a853fc14ef25cb335d841ed4aaa80eb9f
kevinb9n <kevinb9n@gmail.com>
2022-12-29 16:57:46 -0800

    StateChange

---
+++ pets/src/main/kotlin/dev/martianzoo/tfm/pets/ast/StateChange.kt
@@ -0,0 +1,61 @@
+package dev.martianzoo.tfm.pets.ast
+
+data class StateChange(
+    /** Matches this object's position in the game history list, 1-referenced. */
+    val ordinal: Int,
+
+    /**
+     * How many of the component were gained/removed/transmuted. A positive integer. Often
+     * 1, since many component types don't admit duplicates.
+     */
+    val count: Int = 1,
+
+    /** The concrete component that was gained, or `null` if this was a remove. */
+    val gained: TypeExpression? = null,
+
+    /**
+     * The concrete component that was removed, or `null` if this was a gain. Can't be the same as
+     * `gained` (e.g. both can't be null).
+     */
+    val removed: TypeExpression? = null,
+
+    /** Information about what caused this state change, if we have it. */
+    val cause: Cause? = null
+) : PetsNode() {
+
+  init {
+    require(ordinal > 0)
+    require(count > 0)
+    require(gained != removed) { "both gaining and removing $gained" }
+    require((cause?.change ?: 0) < ordinal) { "${cause!!.change} >= $ordinal" }
+  }
+
+  override fun toString(): String {
+    var desc = ""
+    when (gained) {
+      null -> desc += "-$count $removed"
+      else -> {
+        desc += "$count $gained"
+        if (removed != null) desc += " FROM $removed"
+      }
+    }
+
+    val a = cause?.agent ?: "Unknown"
+    val c = cause?.change ?: "Unknown"
+    return "$desc BY $a BECAUSE $c"
+  }
+
+  override val children = listOfNotNull(gained, removed, cause?.agent)
+  override val kind = "StateChange"
+
+  data class Cause(
+      /** The concrete component that owns the instruction that caused this change. */
+      val agent: TypeExpression,
+
+      /** The ordinal of the previous change which triggered that instruction. */
+      val change: Int) {
+    init {
+      require(change > 0)
+    }
+  }
+}
---
+++ pets/src/test/kotlin/dev/martianzoo/tfm/pets/ast/StateChangeTest.kt
@@ -0,0 +1,22 @@
+package dev.martianzoo.tfm.pets.ast
+
+import dev.martianzoo.tfm.pets.ast.StateChange.Cause
+import dev.martianzoo.tfm.pets.ast.TypeExpression.Companion.te
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class StateChangeTest {
+
+  @Test
+  fun bad() {
+    val valid = StateChange(5, 42, te("Foo"), te("Bar"), Cause(te("Qux"), 3))
+
+    assertThrows<RuntimeException> { valid.copy(ordinal = 2) }
+    assertThrows<RuntimeException> { valid.copy(ordinal = 3) }
+    assertThrows<RuntimeException> { valid.copy(count = 0) }
+    assertThrows<RuntimeException> { valid.copy(gained = null, removed = null) }
+    assertThrows<RuntimeException> { valid.copy(gained = te("Same"), removed = te("Same")) }
+    assertThrows<RuntimeException> { valid.copy(cause = valid.cause!!.copy(change = 5)) }
+    assertThrows<RuntimeException> { valid.cause!!.copy(change = 0) }
+  }
+}
