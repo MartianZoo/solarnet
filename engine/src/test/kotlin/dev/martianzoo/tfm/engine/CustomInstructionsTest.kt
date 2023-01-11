7597474bdae42e8277f6a8cfb804d004409f1ab8
kevinb9n <kevinb9n@gmail.com>
2023-01-10 19:45:18 -0800

    Add second custom instruction (Robotic Workforce). Add a Java example of implementing a custom instruction, but dunno how to make it actually build

---
+++ engine/src/test/kotlin/dev/martianzoo/tfm/engine/CustomInstructionsTest.kt
@@ -0,0 +1,91 @@
+package dev.martianzoo.tfm.engine
+
+import com.google.common.truth.Truth.assertThat
+import dev.martianzoo.tfm.canon.Canon
+import dev.martianzoo.tfm.data.ActionDefinition
+import dev.martianzoo.tfm.data.Authority
+import dev.martianzoo.tfm.data.CardDefinition
+import dev.martianzoo.tfm.data.ClassDeclaration
+import dev.martianzoo.tfm.data.MapAreaDefinition
+import dev.martianzoo.tfm.data.MilestoneDefinition
+import dev.martianzoo.tfm.pets.PetsParser.parseScript
+import dev.martianzoo.util.Grid
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+private class CustomInstructionsTest {
+
+  @Test
+  fun robinson() {
+    val game = Engine.newGame(Canon, 3, setOf("B"))
+    val s = """
+      // The standard hack for every player - ignore it!
+      EXEC PROD[5 Megacredit<Player1>]
+
+      EXEC PROD[Steel<Player1>, Titanium<Player1>, Plant<Player1>, Energy<Player1>, Heat<Player1>]
+      EXEC $${""}gainLowestProduction(Player1)
+      COUNT Production<Player1, Megacredit.CLASS> -> foo      
+    """
+
+    assertThat(game.classTable["Player1"].abstract).isFalse()
+    assertThat(game.resolve("Player1").abstract).isFalse()
+    val script = parseScript(s)
+    val results = game.execute(script)
+    assertThat(results["foo"]).isEqualTo(6)
+  }
+
+  @Test
+  fun robinsonCant() {
+    val game = Engine.newGame(Canon, 3, setOf("B"))
+    val s = """
+      // The standard hack for every player - ignore it!
+      EXEC PROD[5 Megacredit<Player1>]
+
+      EXEC PROD[Steel<Player1>, Titanium<Player1>, Plant<Player1>, Heat<Player1>]
+      EXEC $${""}gainLowestProduction(Player1)
+    """
+    val script = parseScript(s)
+    assertThrows<RuntimeException> { game.execute(script) }
+    game.changeLog.forEach(::println)
+  }
+
+  // TODO figure out how to make gradle compile the java code
+  // It seemed like adding plugins { `java-library` } should have been enough
+  fun java() {
+    val auth = object : Authority() {
+      override val explicitClassDeclarations: Collection<ClassDeclaration> = Canon.explicitClassDeclarations
+      override val actionDefinitions: Collection<ActionDefinition> = Canon.actionDefinitions
+      override val cardDefinitions: Collection<CardDefinition> = Canon.cardDefinitions
+      override val mapAreaDefinitions: Map<String, Grid<MapAreaDefinition>> = Canon.mapAreaDefinitions
+      override val milestoneDefinitions: Collection<MilestoneDefinition> = Canon.milestoneDefinitions
+      override fun customInstructions() = listOf(CustomJavaExample.GainLowestProduction())
+    }
+
+    val game = Engine.newGame(auth, 3, setOf("B"))
+    val s = """
+      // The standard hack for every player - ignore it!
+      EXEC PROD[5 Megacredit<Player1>]
+  
+      EXEC PROD[Steel<Player1>, Titanium<Player1>, Plant<Player1>, Energy<Player1>, Heat<Player1>]
+      EXEC $${""}gainLowestProduction(Player1)
+      REQUIRE =6 Production<Player1, Megacredit.CLASS>      
+    """
+
+    game.execute(parseScript(s))
+  }
+
+  @Test
+  fun robinson2() {
+    val game = Engine.newGame(Canon, 3, setOf("B"))
+    val s = """
+      // The standard hack for every player - ignore it!
+      EXEC PROD[5 Megacredit<Player1>]
+
+      EXEC PROD[-Megacredit<Player1>]
+      EXEC $${""}gainLowestProduction(Player1)
+      REQUIRE =5 Production<Player1, Megacredit.CLASS>
+    """
+    val script = parseScript(s)
+    game.execute(script)
+  }
+}
