3308a78a621bcbfd5961ac5d8f404907968960b9
kevinb9n <kevinb9n@gmail.com>
2023-01-16 13:11:50 -0800

    Make up arrow work

--- repl/src/main/kotlin/dev/martianzoo/tfm/repl/main.kt
+++ repl/src/main/kotlin/dev/martianzoo/tfm/repl/main.kt
@@ -3,15 +3,21 @@ package dev.martianzoo.tfm.repl
 import org.jline.reader.EndOfFileException
 import org.jline.reader.LineReaderBuilder
 import org.jline.reader.impl.history.DefaultHistory
+import org.jline.terminal.Attributes
 import org.jline.terminal.TerminalBuilder
+import org.jline.utils.InfoCmp.Capability
 
 fun main() {
-  val term = TerminalBuilder.builder()
+  val terminal = TerminalBuilder.builder()
       .color(true)
       .build()
 
+  terminal.enterRawMode()
+  terminal.puts(Capability.enter_ca_mode)
+  terminal.puts(Capability.keypad_xmit)
+
   val reader = LineReaderBuilder.builder()
-      .terminal(term)
+      .terminal(terminal)
       .history(DefaultHistory())
       .build()
 
