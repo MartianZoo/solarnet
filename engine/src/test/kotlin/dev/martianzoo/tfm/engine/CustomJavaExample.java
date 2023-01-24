d26078f6b5a191f3a70fee8a19b608d3c30d8016
kevinb9n <kevinb9n@gmail.com>
2023-01-24 14:51:00 -0800

    fix file that nothing is compiling

--- engine/src/test/kotlin/dev/martianzoo/tfm/engine/CustomJavaExample.java
+++ engine/src/test/kotlin/dev/martianzoo/tfm/engine/CustomJavaExample.java
@@ -8,7 +8,7 @@ import dev.martianzoo.tfm.api.CustomInstruction;
 import dev.martianzoo.tfm.api.ReadOnlyGameState;
 import dev.martianzoo.tfm.pets.Parsing;
 import dev.martianzoo.tfm.pets.ast.Instruction;
-import dev.martianzoo.tfm.pets.ast.TypeEx;
+import dev.martianzoo.tfm.pets.ast.TypeExpr;
 import java.util.Collections;
 import java.util.List;
 
@@ -22,7 +22,7 @@ public class CustomJavaExample {
 
     @Override
     public Instruction translate( // TODO null annotations
-        ReadOnlyGameState game, List<? extends TypeEx> arguments) {
+        ReadOnlyGameState game, List<? extends TypeExpr> arguments) {
       var player = getOnlyElement(arguments);
       var prods = lookUpProductionLevels(game, player);
       int lowest = Collections.min(prods.values());
