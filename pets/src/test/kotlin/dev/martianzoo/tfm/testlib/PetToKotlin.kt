6fac7051e180d690b461acf7dd32b6dcff67a89d
kevinb9n <kevinb9n@gmail.com>
2023-01-25 00:10:59 -0800

    nest the when statement & remove `else`

--- pets/src/test/kotlin/dev/martianzoo/tfm/testlib/PetToKotlin.kt
+++ pets/src/test/kotlin/dev/martianzoo/tfm/testlib/PetToKotlin.kt
@@ -5,6 +5,7 @@ import dev.martianzoo.tfm.pets.ast.Action.Cost
 import dev.martianzoo.tfm.pets.ast.ClassName
 import dev.martianzoo.tfm.pets.ast.Effect
 import dev.martianzoo.tfm.pets.ast.Effect.Trigger
+import dev.martianzoo.tfm.pets.ast.From
 import dev.martianzoo.tfm.pets.ast.From.ComplexFrom
 import dev.martianzoo.tfm.pets.ast.From.SimpleFrom
 import dev.martianzoo.tfm.pets.ast.From.TypeAsFrom
@@ -18,6 +19,7 @@ import dev.martianzoo.tfm.pets.ast.Instruction.Transmute
 import dev.martianzoo.tfm.pets.ast.PetNode
 import dev.martianzoo.tfm.pets.ast.Requirement
 import dev.martianzoo.tfm.pets.ast.ScalarAndType
+import dev.martianzoo.tfm.pets.ast.TypeExpr
 import dev.martianzoo.tfm.pets.ast.TypeExpr.ClassLiteral
 import dev.martianzoo.tfm.pets.ast.TypeExpr.GenericTypeExpr
 import dev.martianzoo.util.iff
@@ -35,20 +37,22 @@ internal object PetToKotlin {
       return when (this) {
         null -> "null"
         is ClassName -> "cn(\"$this\")"
-        is ClassLiteral -> "${p2k(className)}.literal"
-        is GenericTypeExpr -> {
-          var s = p2k(root)
-          if (args.any()) {
-            s += ".addArgs(${args.join()})"
-          } else {
-            s += ".type"
-          }
-          if (refinement != null) {
-            s += ".refine(${p2k(refinement)})"
-          }
-          s
-        }
-
+        is TypeExpr ->
+            when (this) {
+              is ClassLiteral -> "${p2k(className)}.literal"
+              is GenericTypeExpr -> {
+                var s = p2k(root)
+                if (args.any()) {
+                  s += ".addArgs(${args.join()})"
+                } else {
+                  s += ".type"
+                }
+                if (refinement != null) {
+                  s += ".refine(${p2k(refinement)})"
+                }
+                s
+              }
+            }
         is ScalarAndType -> {
           if (scalar == 1) {
             "sat(${p2k(typeExpr)})"
@@ -56,43 +60,58 @@ internal object PetToKotlin {
             "sat($scalar${p2k(typeExpr).pre(", ")})"
           }
         }
-        is Requirement.Min -> "Min(${p2k(sat)})"
-        is Requirement.Max -> "Max(${p2k(sat)})"
-        is Requirement.Exact -> "Exact(${p2k(sat)})"
-        is Requirement.Or -> "Requirement.Or(${requirements.join()})"
-        is Requirement.And -> "Requirement.And(${requirements.join()})"
-        is Requirement.Transform -> "Requirement.Transform(${p2k(requirement)}, \"$transform\")"
-        is Gain -> "Gain(${p2k(sat)}${intensity.pre(", ")})"
-        is Remove -> "Remove(${p2k(sat)}${intensity.pre(", ")})"
-        is Instruction.Per -> "Instruction.Per(${p2k(instruction)}, ${p2k(sat)})"
-        is Gated -> "Gated(${p2k(gate)}, ${p2k(instruction)})"
-        is Transmute -> "Transmute(${p2k(from)}, $count${intensity.pre(", ")})"
-        is ComplexFrom ->
-            "ComplexFrom(cn(\"$className\"), listOf(${arguments.join()})${refinement.pre(", ")})"
-
-        is SimpleFrom -> "SimpleFrom(${p2k(toType)}, ${p2k(fromType)})"
-        is TypeAsFrom -> "TypeAsFrom(${p2k(typeExpr)})"
-        is Custom ->
-          "Instruction.Custom(\"$functionName\"" +
-              "${arguments.joinToString("") { ", ${p2k(it)}" }})"
-
-        is Then -> "Then(${instructions.join()})"
-        is Instruction.Or -> "Instruction.Or(${instructions.join()})"
-        is Instruction.Multi -> "Instruction.Multi(${instructions.join()})"
-        is Instruction.Transform -> "Instruction.Transform(${p2k(instruction)}, \"$transform\")"
-        is Trigger.OnGain -> "OnGain(${p2k(typeExpr)})"
-        is Trigger.OnRemove -> "OnRemove(${p2k(typeExpr)})"
-        is Trigger.Transform -> "Trigger.Transform(${p2k(trigger)}, \"$transform\")"
+        is Requirement -> {
+          when (this) {
+            is Requirement.Min -> "Min(${p2k(sat)})"
+            is Requirement.Max -> "Max(${p2k(sat)})"
+            is Requirement.Exact -> "Exact(${p2k(sat)})"
+            is Requirement.Or -> "Requirement.Or(${requirements.join()})"
+            is Requirement.And -> "Requirement.And(${requirements.join()})"
+            is Requirement.Transform -> "Requirement.Transform(${p2k(requirement)}, \"$transform\")"
+          }
+        }
+        is Instruction -> {
+          when (this) {
+            is Gain -> "Gain(${p2k(sat)}${intensity.pre(", ")})"
+            is Remove -> "Remove(${p2k(sat)}${intensity.pre(", ")})"
+            is Instruction.Per -> "Instruction.Per(${p2k(instruction)}, ${p2k(sat)})"
+            is Gated -> "Gated(${p2k(gate)}, ${p2k(instruction)})"
+            is Transmute -> "Transmute(${p2k(from)}, $count${intensity.pre(", ")})"
+            is Custom ->
+                "Instruction.Custom(\"$functionName\"" +
+                    "${arguments.joinToString("") { ", ${p2k(it)}" }})"
+            is Then -> "Then(${instructions.join()})"
+            is Instruction.Or -> "Instruction.Or(${instructions.join()})"
+            is Instruction.Multi -> "Instruction.Multi(${instructions.join()})"
+            is Instruction.Transform -> "Instruction.Transform(${p2k(instruction)}, \"$transform\")"
+          }
+        }
+        is From -> {
+          when (this) {
+            is ComplexFrom ->
+                "ComplexFrom(cn(\"$className\"), listOf(${arguments.join()})${refinement.pre(", ")})"
+            is SimpleFrom -> "SimpleFrom(${p2k(toType)}, ${p2k(fromType)})"
+            is TypeAsFrom -> "TypeAsFrom(${p2k(typeExpr)})"
+          }
+        }
+        is Trigger -> {
+          when (this) {
+            is Trigger.OnGain -> "OnGain(${p2k(typeExpr)})"
+            is Trigger.OnRemove -> "OnRemove(${p2k(typeExpr)})"
+            is Trigger.Transform -> "Trigger.Transform(${p2k(trigger)}, \"$transform\")"
+          }
+        }
         is Effect -> "Effect(${p2k(trigger)}, ${p2k(instruction)}${", true".iff(automatic)})"
-        is Cost.Spend -> "Spend(${p2k(sat)})"
-        is Cost.Per -> "Cost.Per(${p2k(cost)}, ${p2k(sat)})"
-        is Cost.Or -> "Cost.Or(${costs.join()})"
-        is Cost.Multi -> "Cost.Multi(${costs.join()})"
-        is Cost.Transform -> "Cost.Transform(${p2k(cost)}, \"$transform\")"
+        is Cost -> {
+          when (this) {
+            is Cost.Spend -> "Spend(${p2k(sat)})"
+            is Cost.Per -> "Cost.Per(${p2k(cost)}, ${p2k(sat)})"
+            is Cost.Or -> "Cost.Or(${costs.join()})"
+            is Cost.Multi -> "Cost.Multi(${costs.join()})"
+            is Cost.Transform -> "Cost.Transform(${p2k(cost)}, \"$transform\")"
+          }
+        }
         is Action -> "Action(${p2k(cost)}, ${p2k(instruction)})"
-
-        // I can't figure out wtf is missing... and all the classes are sealed
-        else -> error("")
       }
     }
   }
