5aff62aed966f30c847d668de4b0a587a68a54c6
kevinb9n <kevinb9n@gmail.com>
2023-01-27 15:41:18 -0800

    Dependency was an interface, which was slightly more annoying.

--- engine/src/main/kotlin/dev/martianzoo/tfm/types/Dependency.kt
+++ engine/src/main/kotlin/dev/martianzoo/tfm/types/Dependency.kt
@@ -2,14 +2,15 @@ package dev.martianzoo.tfm.types
 
 import dev.martianzoo.tfm.pets.ast.TypeExpr
 
-interface Dependency {
-  val key: Key
-  val abstract: Boolean
-  fun checkKeys(that: Dependency): Dependency
-  fun specializes(that: Dependency): Boolean
-  fun intersect(that: Dependency): Dependency?
+abstract class Dependency {
+  abstract val key: Key
+  abstract val abstract: Boolean
+  abstract fun specializes(that: Dependency): Boolean
+  abstract fun intersect(that: Dependency): Dependency?
 
-  fun toTypeExprFull(): TypeExpr
+  abstract fun toTypeExprFull(): TypeExpr
+
+  override fun toString() = "$key=${toTypeExprFull()}"
 
   public data class Key(val declaringClass: PClass, val index: Int) {
     init {
@@ -19,10 +20,10 @@ interface Dependency {
     override fun toString() = "${declaringClass.name}_$index"
   }
 
-  public data class TypeDependency(override val key: Key, val ptype: PType) : Dependency {
+  public data class TypeDependency(override val key: Key, val ptype: PType) : Dependency() {
     override val abstract by ptype::abstract
 
-    override fun checkKeys(that: Dependency): TypeDependency {
+    private fun checkKeys(that: Dependency): TypeDependency {
       require(this.key == that.key)
       return that as TypeDependency
     }
@@ -35,31 +36,26 @@ interface Dependency {
         (this.ptype intersect otherType)?.let { copy(ptype = it) }
 
     override fun toTypeExprFull() = ptype.toTypeExprFull()
-
-    override fun toString() = "$key=${toTypeExprFull()}"
   }
 
   /** Okay this is used ONLY by Class_0, and the value is just a class, like just Tile.
    * */
-  public data class ClassDependency(override val key: Key, val pclass: PClass) : Dependency {
+  public data class ClassDependency(override val key: Key, val pclass: PClass) : Dependency() {
     override val abstract by pclass::abstract
-
-    override fun checkKeys(that: Dependency): ClassDependency {
-      require(this.key == that.key)
-      return that as ClassDependency
-    }
 
     override fun specializes(that: Dependency) = pclass.isSubclassOf(checkKeys(that).pclass)
 
     override fun intersect(that: Dependency): ClassDependency? =
         this intersect checkKeys(that).pclass
 
-    infix fun intersect(otherType: PClass): ClassDependency? =
-        (this.pclass intersect otherType)?.let { copy(pclass = it) }
+    infix fun intersect(otherClass: PClass): ClassDependency? =
+        (this.pclass intersect otherClass)?.let { copy(pclass = it) }
 
     override fun toTypeExprFull() = pclass.name.type
 
-    override fun toString() = "$key=${toTypeExprFull()}"
+    private fun checkKeys(that: Dependency): ClassDependency {
+      require(this.key == that.key)
+      return that as ClassDependency
+    }
   }
-
 }
