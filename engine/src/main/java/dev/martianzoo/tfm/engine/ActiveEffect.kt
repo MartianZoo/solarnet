83ecb40ad73918e6d0abb7e259486faf23b4b4d8
kevinb9n <kevinb9n@gmail.com>
2023-03-16 00:53:17 -0700

    little refactoring

--- engine/src/main/java/dev/martianzoo/tfm/engine/ActiveEffect.kt
+++ engine/src/main/java/dev/martianzoo/tfm/engine/ActiveEffect.kt
@@ -13,18 +13,14 @@ data class ActiveEffect(
     val instruction: Instruction,
 ) {
   companion object {
-    fun from(it: Effect, contextComponent: Component) =
-        ActiveEffect(contextComponent, ActiveTrigger.from(it.trigger), it.automatic, it.instruction)
+    fun from(it: Effect, context: Component) =
+        ActiveEffect(context, ActiveTrigger.from(it.trigger), it.automatic, it.instruction)
   }
 
   fun onChange(triggerEvent: ChangeEvent, game: Game, isSelf: Boolean): FiredEffect? {
     val actor = context.owner()?.let(::Actor) ?: triggerEvent.actor
     val hit = trigger.match(triggerEvent, actor, game, isSelf) ?: return null
-    return FiredEffect(hit.modify(instruction), actor, triggeredBy(triggerEvent), automatic)
-  }
-
-  private fun triggeredBy(triggerEvent: ChangeEvent): Cause { // TODO check
-    return Cause(context, triggerEvent)
+    return FiredEffect(hit(instruction), actor, Cause(context, triggerEvent), automatic)
   }
 
   data class FiredEffect(
--- engine/src/main/java/dev/martianzoo/tfm/engine/ActiveTrigger.kt
+++ engine/src/main/java/dev/martianzoo/tfm/engine/ActiveTrigger.kt
@@ -17,14 +17,16 @@ import dev.martianzoo.tfm.pets.ast.Instruction
 import dev.martianzoo.tfm.pets.ast.Requirement
 import dev.martianzoo.tfm.types.Transformers.ReplaceOwnerWith
 
+typealias Hit = (Instruction) -> Instruction
+
 sealed class ActiveTrigger {
   companion object {
     fun from(trigger: Trigger): ActiveTrigger {
       return when (trigger) {
         is ByTrigger -> ByActor(from(trigger.inner), trigger.by)
         is IfTrigger -> Conditional(from(trigger.inner), trigger.condition)
-        is WhenGain -> MatchOnSelf(true)
-        is WhenRemove -> MatchOnSelf(false)
+        is WhenGain -> MatchOnSelf(matchOnGain = true)
+        is WhenRemove -> MatchOnSelf(matchOnGain = false)
         is OnGainOf -> MatchOnOthers(trigger.expression, matchOnGain = true)
         is OnRemoveOf -> MatchOnOthers(trigger.expression, matchOnGain = false)
         is Transform -> error("should have been transformed by now")
@@ -34,6 +36,7 @@ sealed class ActiveTrigger {
 
   class Conditional(val inner: ActiveTrigger, val condition: Requirement) : ActiveTrigger() {
     override fun match(triggerEvent: ChangeEvent, actor: Actor, game: Game, isSelf: Boolean): Hit? {
+      // This sort of feels out of order, but I don't think that hurts anything
       return if (game.evaluate(condition)) {
         inner.match(triggerEvent, actor, game, isSelf)
       } else {
@@ -51,12 +54,7 @@ sealed class ActiveTrigger {
       val originalHit = inner.match(triggerEvent, actor, game, isSelf) ?: return null
 
       return if (by == OWNER) {
-        object : Hit {
-          override fun modify(instruction: Instruction): Instruction {
-            // TODO which way
-            return ReplaceOwnerWith(actor.className).transform(originalHit.modify(instruction))
-          }
-        }
+        { ReplaceOwnerWith(actor.className).transform(originalHit(it)) }
       } else {
         originalHit
       }
@@ -70,7 +68,7 @@ sealed class ActiveTrigger {
       if (!isSelf) return null
       val isThisAGain = (triggerEvent.change.gaining != null)
       return if (isThisAGain == matchOnGain) {
-        genericHit(triggerEvent.change.count)
+        { it * triggerEvent.change.count }
       } else {
         null
       }
@@ -83,21 +81,10 @@ sealed class ActiveTrigger {
       val change = triggerEvent.change
       val expr = (if (matchOnGain) change.gaining else change.removing) ?: return null
       return if (game.resolve(expr).isSubtypeOf(game.resolve(match))) {
-        genericHit(change.count)
+        { it * change.count }
       } else {
         null
       }
     }
   }
-
-  interface Hit {
-    fun modify(instruction: Instruction): Instruction
-  }
-
-  fun genericHit(count: Int): Hit =
-      object : Hit {
-        override fun modify(instruction: Instruction): Instruction {
-          return instruction * count
-        }
-      }
 }
