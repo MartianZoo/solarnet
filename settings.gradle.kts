b8df23dae80d17a7b2de2bc6630b89825b018a88
Michael Diamond <dimo414@gmail.com>
2023-04-28 21:18:50 -0700

    Initialize a GitHub CI action.
    Runs `gradle build` and persists the repl-all.jar artifact.
    
    See the comments in gradle.yml as well as https://docs.gradle.org/current/userguide/github-actions.html

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,18 @@
+// Enable Build Scans
+// https://docs.gradle.org/current/userguide/github-actions.html#enable_build_scan_publishing
+plugins {
+    id("com.gradle.enterprise") version("3.9")
+}
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
+
 rootProject.name = "solarnet"
 
 include("pets", "engine", "repl", "canon")
