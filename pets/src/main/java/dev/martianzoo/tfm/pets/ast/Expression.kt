c4bc86872e9c2d6cfb66209c7743ce4f5a1e5a20
kevinb9n <kevinb9n@gmail.com>
2023-05-25 22:08:52 -0700

    minor doc/refac

--- pets/src/main/java/dev/martianzoo/tfm/pets/ast/Expression.kt
+++ pets/src/main/java/dev/martianzoo/tfm/pets/ast/Expression.kt
@@ -6,37 +6,39 @@ import com.github.h0tk3y.betterParse.combinators.optional
 import com.github.h0tk3y.betterParse.combinators.skip
 import com.github.h0tk3y.betterParse.grammar.parser
 import com.github.h0tk3y.betterParse.parser.Parser
+import dev.martianzoo.tfm.api.Type
 import dev.martianzoo.tfm.pets.HasClassName
 import dev.martianzoo.tfm.pets.HasExpression
 import dev.martianzoo.tfm.pets.PetTokenizer
-import dev.martianzoo.util.joinOrEmpty
-import dev.martianzoo.util.wrap
 
 /**
- * A noun expression in Pets; a particular representation of a type. Could be a simple type (`Foo`),
- * a type with arguments (`Foo<Bar, Qux>`), or a refined type (`Foo<Bar(HAS 3 Qux)>(HAS Wau)`) (the
- * combination of a real type with a [Requirement]).
+ * A noun expression in Pets language, which is a particular *representation* of a type. An
+ * expression might have arguments (as in `Microbe<Player1, Ants>`), where each (like `Ants`) is
+ * itself an expression. It also might have a refinement (as in `Card(HAS VenusTag)`), which is of
+ * type [Requirement]. (It could have either, neither, or both.)
  *
- * **Caution:** in many cases different expressions can represent the same "actual" type; for
- * example `Microbe<This, Player1>` and `Microbe<Player1, This`, or for another example, `Tile` and
- * `Tile<Area>`. This class has no idea about that; they are different *representations* so they are
- * considered unequal.
+ * Many types can have different representations; for example `Microbe<This, Player1>` and
+ * `Microbe<Player1, This>` represent the same actual type, as do `Tile` and `Tile<Area>`. As
+ * [Expression]s these four example types are all distinct, which could produce unexpected behavior.
+ * A class in the *engine* module (`MClassLoader`) resolves expressions into [Type] instances, and
+ * does resolve the distinct expressions `Tile` and `Tile<Area>` into the same type.
  */
 public data class Expression(
     override val className: ClassName,
     val arguments: List<Expression> = listOf(),
     val refinement: Requirement? = null,
 ) : PetElement(), HasClassName {
+
   override fun visitChildren(visitor: Visitor) =
       visitor.visit(listOf(className) + arguments + refinement)
 
-  override fun toString() =
-      "$className" + arguments.joinOrEmpty(wrap = "<>") + refinement.wrap("(HAS ", ")")
+  override fun toString() = buildString {
+    append(className)
+    if (arguments.any()) append(arguments.joinToString(", ", "<", ">"))
+    if (refinement != null) append("(HAS $refinement)")
+  }
 
-  /**
-   * Is this just `ClassName.expression` for some class name? (That is, no arguments, no
-   * refinement?)
-   */
+  /** Does this expression consist only of a class name, with no arguments and no refinement? */
   val simple: Boolean = arguments.isEmpty() && refinement == null
 
   @JvmName("extractAndAppendArguments")
@@ -54,27 +56,28 @@ public data class Expression(
   fun replaceArguments(newArgs: List<Expression>): Expression = copy(arguments = newArgs)
   fun replaceArguments(vararg newArgs: Expression): Expression = replaceArguments(newArgs.toList())
 
-  /** Returns this expression (which must have no refinement already), plus the given refinement. */
-  fun has(refinement: Requirement?) =
-      if (refinement == null) {
-        this
-      } else {
-        require(this.refinement == null)
-        copy(refinement = refinement)
-      }
+  /**
+   * Returns this expression with the given refinement. This expression must not already have a
+   * refinement.
+   */
+  fun has(refinement: Requirement?): Expression {
+    require(this.refinement == null)
+    return if (refinement != null) copy(refinement = refinement) else this
+  }
 
   override val kind = Expression::class
 
   internal companion object : PetTokenizer() {
     fun parser(): Parser<Expression> {
       return parser {
-        val specs = skipChar('<') and commaSeparated(parser()) and skipChar('>')
+        val argumentList = skipChar('<') and commaSeparated(parser()) and skipChar('>')
+        val refinement = group(skip(_has) and Requirement.parser())
 
         ClassName.parser() and
-            optionalList(specs) and
-            optional(group(skip(_has) and Requirement.parser())) map { (clazz, args, ref) ->
-          Expression(clazz, args, ref)
-        }
+            optionalList(argumentList) and
+            optional(refinement) map { (clazz, args, ref) ->
+              Expression(clazz, args, ref)
+            }
       }
     }
   }
