d602757c6df10c9c8388f3b9a3fde7e1a2011d16
kevinb9n <kevinb9n@gmail.com>
2023-06-04 12:14:34 -0700

    port Robinson test

--- engine/src/test/java/dev/martianzoo/tfm/engine/cards/RobinsonIndustriesTest.kt
+++ engine/src/test/java/dev/martianzoo/tfm/engine/cards/RobinsonIndustriesTest.kt
@@ -6,12 +6,9 @@ import dev.martianzoo.tfm.canon.Canon
 import dev.martianzoo.tfm.data.GameSetup
 import dev.martianzoo.tfm.data.Player.Companion.PLAYER1
 import dev.martianzoo.tfm.engine.Engine
-import dev.martianzoo.tfm.engine.OldTfmHelpers.cardAction1
-import dev.martianzoo.tfm.engine.OldTfmHelpers.phase
-import dev.martianzoo.tfm.engine.OldTfmHelpers.playCorp
-import dev.martianzoo.tfm.engine.OldTfmHelpers.production
-import dev.martianzoo.tfm.engine.PlayerSession
-import dev.martianzoo.tfm.engine.PlayerSession.Companion.session
+import dev.martianzoo.tfm.engine.TerraformingMarsApi.Companion.tfm
+import dev.martianzoo.tfm.engine.TestHelpers.assertProds
+import dev.martianzoo.tfm.engine.Timeline.AbortOperationException
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -20,72 +17,73 @@ class RobinsonIndustriesTest {
 
   @Test
   fun megacredit1() {
-    with(game.session(PLAYER1)) {
+    with(game.tfm(PLAYER1)) {
       playCorp("RobinsonIndustries", 0)
-      assertThat(count("Megacredit")).isEqualTo(47)
+      assertThat(gameplay.count("Megacredit")).isEqualTo(47)
 
-      writer.sneak("PROD[S, T, P, E, H]")
-      checkProduction(0, 1, 1, 1, 1, 1)
+      sneak("PROD[S, T, P, E, H]")
+      assertProds(0 to "M", 1 to "S", 1 to "T", 1 to "P", 1 to "E", 1 to "H")
 
       phase("Action")
       cardAction1("RobinsonIndustries")
-      assertThat(count("Megacredit")).isEqualTo(43)
-      checkProduction(1, 1, 1, 1, 1, 1)
+      assertThat(gameplay.count("Megacredit")).isEqualTo(43)
+      assertProds(1 to "M", 1 to "S", 1 to "T", 1 to "P", 1 to "E", 1 to "H")
     }
   }
 
   @Test
   fun megacredit2() {
-    val p1 = game.session(PLAYER1)
+    with(game.tfm(PLAYER1)) {
+      playCorp("RobinsonIndustries", 0)
+      sneak("PROD[-1]")
+      assertProds(-1 to "M", 0 to "S", 0 to "T", 0 to "P", 0 to "E", 0 to "H")
 
-    p1.playCorp("RobinsonIndustries", 0)
-    p1.writer.sneak("PROD[-1]")
-    p1.checkProduction(-1, 0, 0, 0, 0, 0)
-
-    p1.phase("Action")
-    p1.cardAction1("RobinsonIndustries")
-    p1.checkProduction(0, 0, 0, 0, 0, 0)
+      phase("Action")
+      cardAction1("RobinsonIndustries")
+      assertProds(0 to "M", 0 to "S", 0 to "T", 0 to "P", 0 to "E", 0 to "H")
+    }
   }
 
   @Test
   fun nonMegacredit() {
-    val p1 = game.session(PLAYER1)
-
-    p1.playCorp("RobinsonIndustries", 0)
-    p1.writer.sneak("PROD[1, S, P, E, H]")
-    p1.checkProduction(1, 1, 0, 1, 1, 1)
-
-    p1.phase("Action")
-    p1.cardAction1("RobinsonIndustries")
-    p1.checkProduction(1, 1, 1, 1, 1, 1)
+    with(game.tfm(PLAYER1)) {
+      playCorp("RobinsonIndustries", 0)
+      sneak("PROD[1, S, P, E, H]")
+      assertProds(1 to "M", 1 to "S", 0 to "T", 1 to "P", 1 to "E", 1 to "H")
+      phase("Action")
+      cardAction1("RobinsonIndustries")
+      assertProds(1 to "M", 1 to "S", 1 to "T", 1 to "P", 1 to "E", 1 to "H")
+    }
   }
 
   @Test
   fun choice() {
-    with(game.session(PLAYER1)) {
+    with(game.tfm(PLAYER1)) {
       playCorp("RobinsonIndustries", 0)
-      writer.sneak("PROD[S, P, E, H]")
-      checkProduction(0, 1, 0, 1, 1, 1)
+      sneak("PROD[S, P, E, H]")
+      assertProds(0 to "M", 1 to "S", 0 to "T", 1 to "P", 1 to "E", 1 to "H")
 
       phase("Action")
       cardAction1("RobinsonIndustries") {
         assertThat(tasks.extract { "${it.instruction}" })
             .containsExactly(
                 "Production<Player1, Class<Megacredit>>! OR Production<Player1, Class<Titanium>>!")
-        task("PROD[1]")
-        checkProduction(1, 1, 0, 1, 1, 1)
-        abortAndRollBack()
+        doTask("PROD[1]")
+        assertProds(1 to "M", 1 to "S", 0 to "T", 1 to "P", 1 to "E", 1 to "H")
+        throw AbortOperationException()
       }
 
-      cardAction1("RobinsonIndustries", "PROD[T]") {
-        checkProduction(0, 1, 1, 1, 1, 1)
-        abortAndRollBack()
+      cardAction1("RobinsonIndustries") {
+        doTask("PROD[T]")
+        assertProds(0 to "M", 1 to "S", 1 to "T", 1 to "P", 1 to "E", 1 to "H")
+        throw AbortOperationException()
       }
 
-      assertThrows<NarrowingException> { cardAction1("RobinsonIndustries", "PROD[Steel]") }
+      assertThrows<NarrowingException> {
+        cardAction1("RobinsonIndustries") {
+          doTask("PROD[Steel]")
+        }
+      }
     }
   }
-
-  private fun PlayerSession.checkProduction(vararg exp: Int) =
-      assertThat(production().values).containsExactlyElementsIn(exp.toList()).inOrder()
 }
