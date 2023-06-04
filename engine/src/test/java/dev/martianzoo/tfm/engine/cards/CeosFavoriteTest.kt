7aac648f77f1581afa7e79d824972f21ebc78022
kevinb9n <kevinb9n@gmail.com>
2023-06-04 08:10:47 -0700

    Converting 4th & 5th .cards tests just worked!

--- engine/src/test/java/dev/martianzoo/tfm/engine/cards/CeosFavoriteTest.kt
+++ engine/src/test/java/dev/martianzoo/tfm/engine/cards/CeosFavoriteTest.kt
@@ -5,7 +5,6 @@ import dev.martianzoo.tfm.canon.Canon
 import dev.martianzoo.tfm.data.GameSetup
 import dev.martianzoo.tfm.data.Player.Companion.PLAYER1
 import dev.martianzoo.tfm.engine.Engine
-import dev.martianzoo.tfm.engine.PlayerSession.Companion.session
 import dev.martianzoo.tfm.engine.TestHelpers.assertCounts
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -15,18 +14,22 @@ class CeosFavoriteTest {
   fun ceosFavoriteProject() {
     val game = Engine.newGame(GameSetup(Canon, "CVERB", 2))
 
-    with(game.session(PLAYER1)) {
-      operation("10 ProjectCard, ForcedPrecipitation")
+    with(game.gameplay(PLAYER1).turnLayer().operationLayer()) {
+      initiate("10 ProjectCard, ForcedPrecipitation")
 
       // We can't CEO's onto an empty card
       assertThrows<DependencyException> {
-        operation("CeosFavoriteProject", "Floater<ForcedPrecipitation>")
+        initiate("CeosFavoriteProject") {
+          doTask("Floater<ForcedPrecipitation>")
+        }
       }
 
-      writer.sneak("Floater<ForcedPrecipitation>")
+      taskLayer().changeLayer().sneak("Floater<ForcedPrecipitation>")
       assertCounts(1 to "Floater")
 
-      operation("CeosFavoriteProject", "Floater<ForcedPrecipitation>")
+      initiate("CeosFavoriteProject") {
+        doTask("Floater<ForcedPrecipitation>")
+      }
       assertCounts(2 to "Floater")
     }
   }
--- engine/src/test/java/dev/martianzoo/tfm/engine/cards/CommunityServicesTest.kt
+++ engine/src/test/java/dev/martianzoo/tfm/engine/cards/CommunityServicesTest.kt
@@ -5,8 +5,7 @@ import dev.martianzoo.tfm.canon.Canon
 import dev.martianzoo.tfm.data.GameSetup
 import dev.martianzoo.tfm.data.Player.Companion.PLAYER1
 import dev.martianzoo.tfm.engine.Engine
-import dev.martianzoo.tfm.engine.PlayerSession.Companion.session
-import dev.martianzoo.tfm.engine.OldTfmHelpers.production
+import dev.martianzoo.tfm.engine.TerraformingMarsApi
 import dev.martianzoo.tfm.pets.ast.ClassName.Companion.cn
 import org.junit.jupiter.api.Test
 
@@ -14,15 +13,17 @@ class CommunityServicesTest {
   @Test
   fun communityServices() {
     val game = Engine.newGame(GameSetup(Canon, "CVERB", 2))
-    with(game.session(PLAYER1)) {
-      operation("10 ProjectCard, ForcedPrecipitation")
-      operation("AtmoCollectors", "2 Floater<AtmoCollectors>")
-      operation("Airliners", "2 Floater<AtmoCollectors>")
+    val p1 = TerraformingMarsApi(game, PLAYER1)
 
-      assertThat(production(cn("M"))).isEqualTo(2)
+    with(game.gameplay(PLAYER1).turnLayer().operationLayer()) {
+      initiate("10 ProjectCard, ForcedPrecipitation")
+      initiate("AtmoCollectors") { doTask("2 Floater<AtmoCollectors>") }
+      initiate("Airliners") { doTask("2 Floater<AtmoCollectors>") }
 
-      operation("CommunityServices") // 3 tagless cards: Atmo, Airl, Comm
-      assertThat(production(cn("M"))).isEqualTo(5)
+      assertThat(p1.production(cn("M"))).isEqualTo(2)
+
+      initiate("CommunityServices") // 3 tagless cards: Atmo, Airl, Comm
+      assertThat(p1.production(cn("M"))).isEqualTo(5)
     }
   }
 }
