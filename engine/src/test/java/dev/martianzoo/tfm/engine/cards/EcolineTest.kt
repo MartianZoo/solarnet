e04755748d75f565eb89b1f08d2682e2d77725c1
kevinb9n <kevinb9n@gmail.com>
2023-06-06 01:34:15 -0700

    How about making Ecoline and ThorGate actually work?
    They do cheat a bit.

---
+++ engine/src/test/java/dev/martianzoo/tfm/engine/cards/EcolineTest.kt
@@ -0,0 +1,41 @@
+package dev.martianzoo.tfm.engine.cards
+
+import dev.martianzoo.tfm.api.Exceptions.LimitsException
+import dev.martianzoo.tfm.canon.Canon
+import dev.martianzoo.tfm.data.Player.Companion.PLAYER1
+import dev.martianzoo.tfm.engine.Engine
+import dev.martianzoo.tfm.engine.TestHelpers.assertCounts
+import dev.martianzoo.tfm.engine.TfmGameplay.Companion.tfm
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class EcolineTest {
+  @Test
+  fun test() {
+    val game = Engine.newGame(Canon.SIMPLE_GAME)
+
+    with(game.tfm(PLAYER1)) {
+      phase("Corporation")
+      playCorp("Ecoline", 5)
+      assertCounts(3 to "Plant")
+
+      godMode().sneak("8 Plant")
+      assertCounts(11 to "Plant")
+
+      phase("Action")
+      godMode().manual("CityTile<M52>")
+      assertCounts(13 to "Plant")
+
+      stdAction("ConvertPlantsSA") { doTask("GreeneryTile<M42>") }
+      assertCounts(7 to "Plant")
+
+      stdAction("ConvertPlantsSA") {
+        doTask("GreeneryTile<M32>")
+      }
+      assertCounts(0 to "Plant")
+
+      godMode().sneak("6 Plant")
+      assertThrows<LimitsException> { stdAction("ConvertPlantsSA") }
+    }
+  }
+}
---
+++ engine/src/test/java/dev/martianzoo/tfm/engine/cards/ThorgateTest.kt
@@ -0,0 +1,31 @@
+package dev.martianzoo.tfm.engine.cards
+
+import dev.martianzoo.tfm.api.Exceptions.LimitsException
+import dev.martianzoo.tfm.canon.Canon
+import dev.martianzoo.tfm.data.Player.Companion.PLAYER1
+import dev.martianzoo.tfm.engine.Engine
+import dev.martianzoo.tfm.engine.TestHelpers.assertCounts
+import dev.martianzoo.tfm.engine.TfmGameplay.Companion.tfm
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class ThorgateTest {
+  @Test
+  fun test() {
+    val game = Engine.newGame(Canon.SIMPLE_GAME)
+
+    with(game.tfm(PLAYER1)) {
+      phase("Corporation")
+      playCorp("Thorgate", 10)
+      godMode().sneak("-10")
+
+      phase("Action")
+      assertCounts(8 to "M", 1 to "Production<Class<E>>")
+      stdProject("PowerPlantSP")
+      assertCounts(0 to "M", 2 to "Production<Class<E>>")
+
+      godMode().sneak("7")
+      assertThrows<LimitsException> { stdProject("PowerPlantSP") }
+    }
+  }
+}
