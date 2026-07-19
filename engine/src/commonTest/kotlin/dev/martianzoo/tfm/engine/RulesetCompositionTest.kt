51328ca6586673012a6b6de742b6737ebdd3b2b9
Kevin Bourrillion <kevinb9n@gmail.com>
2026-07-19 09:20:35 -0700

    Use an additive ruleset composition fixture

--- engine/src/commonTest/kotlin/dev/martianzoo/tfm/engine/RulesetCompositionTest.kt
+++ engine/src/commonTest/kotlin/dev/martianzoo/tfm/engine/RulesetCompositionTest.kt
@@ -2,33 +2,29 @@ package dev.martianzoo.tfm.engine
 
 import dev.martianzoo.data.Player.Companion.PLAYER1
 import dev.martianzoo.engine.Engine
+import dev.martianzoo.pets.Parsing.parseOneLinerClass
+import dev.martianzoo.pets.ast.ClassName.Companion.cn
 import dev.martianzoo.tfm.api.TfmRuleset
 import dev.martianzoo.tfm.canon.Canon
 import dev.martianzoo.tfm.data.GameSetup
+import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
 import kotlin.test.Test
 
 internal class RulesetCompositionTest {
   @Test
   fun composedRulesetCreatesAWorkingGame() {
-    val declarations =
+    val extension =
         object : TfmRuleset.Empty() {
-          override val explicitClassDeclarations = Canon.explicitClassDeclarations
-          override val customClasses = Canon.customClasses
+          override val explicitClassDeclarations =
+              setOf(parseOneLinerClass("CLASS CompositionProbe : AutoLoad"))
         }
-    val definitions =
-        object : TfmRuleset.Empty() {
-          override val cardDefinitions = Canon.cardDefinitions
-          override val marsMapDefinitions = Canon.marsMapDefinitions
-          override val milestoneDefinitions = Canon.milestoneDefinitions
-          override val colonyTileDefinitions = Canon.colonyTileDefinitions
-          override val standardActionDefinitions = Canon.standardActionDefinitions
-        }
-    val ruleset = TfmRuleset.compose(declarations, definitions)
+    val ruleset = TfmRuleset.compose(Canon, extension)
 
     val game = Engine.newGame(GameSetup(ruleset, "BM", 2))
 
     game.reader.ruleset shouldBe ruleset
+    game.classes.allClassNamesAndIds.shouldContain(cn("CompositionProbe"))
     game.gameplay(PLAYER1).count("TerraformRating<Player1>") shouldBe 20
   }
 }
