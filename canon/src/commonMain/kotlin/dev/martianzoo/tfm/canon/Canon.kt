1f0041c0875bdd25bb3fc795d6f47d4a62f2ca89
Kevin Bourrillion <kevinb9n@gmail.com>
2026-07-19 17:29:52 -0700

    A coarse clumsy split and move of a whole bunch of resource files (json and pets) to organize them in a way that makes sense for the proper bundlization to come.  Just to defray the massive diffs that are coming.

--- canon/src/commonMain/kotlin/dev/martianzoo/tfm/canon/Canon.kt
+++ canon/src/commonMain/kotlin/dev/martianzoo/tfm/canon/Canon.kt
@@ -19,15 +19,48 @@ import dev.martianzoo.util.toSetStrict
  */
 public object Canon : TfmAuthority() {
   private val PETS_FILENAMES =
-      setOf(
+      listOf(
           "system.pets",
           "global.pets",
-          "maps-tiles.pets",
           "player.pets",
-          "cards.pets",
-          "actions.pets",
-          "payment.pets",
-          "colonies.pets",
+          "bundles/TerraformingMars/maps-tiles.pets",
+          "bundles/TerraformingMars/cards.pets",
+          "bundles/TerraformingMars/actions.pets",
+          "bundles/TerraformingMars/payment.pets",
+          "bundles/ColoniesExpansion/colonies.pets",
+      )
+
+  private val CARD_FILENAMES =
+      listOf(
+          "bundles/TerraformingMars/cards.json5",
+          "bundles/CorporateEraExpansion/cards.json5",
+          "bundles/VenusNextExpansion/cards.json5",
+          "bundles/PreludeExpansion/cards.json5",
+          "bundles/ColoniesExpansion/cards.json5",
+          "bundles/TurmoilExpansion/cards.json5",
+          "bundles/PromoCardsBundle/cards.json5",
+      )
+
+  private val ACTION_FILENAMES =
+      listOf(
+          "bundles/TerraformingMars/actions.json5",
+          "bundles/VenusNextExpansion/actions.json5",
+          "bundles/ColoniesExpansion/actions.json5",
+      )
+
+  private val MAP_FILENAMES =
+      listOf(
+          "bundles/TharsisMap/maps.json5",
+          "bundles/HellasMap/maps.json5",
+          "bundles/ElysiumMap/maps.json5",
+      )
+
+  private val MILESTONE_FILENAMES =
+      listOf(
+          "bundles/TharsisMap/milestones.json5",
+          "bundles/HellasMap/milestones.json5",
+          "bundles/ElysiumMap/milestones.json5",
+          "bundles/VenusNextExpansion/milestones.json5",
       )
 
   override val explicitClassDeclarations: Set<ClassDeclaration> by lazy {
@@ -35,28 +68,34 @@ public object Canon : TfmAuthority() {
   }
 
   override val cardDefinitions: Set<CardDefinition> by lazy {
-    JsonReader.readCards(readResource("cards.json5")).toSetStrict(::CardDefinition)
+    readResources(CARD_FILENAMES, JsonReader::readCards).toSetStrict(::CardDefinition)
   }
 
   override val standardActionDefinitions: Set<StandardActionDefinition> by lazy {
-    JsonReader.readActions(readResource("actions.json5")).toSetStrict()
+    readResources(ACTION_FILENAMES, JsonReader::readActions).toSetStrict()
   }
 
   override val marsMapDefinitions: Set<MarsMapDefinition> by lazy {
-    JsonReader.readMaps(readResource("maps.json5")).toSetStrict()
+    readResources(MAP_FILENAMES, JsonReader::readMaps).toSetStrict()
   }
 
   override val milestoneDefinitions: Set<MilestoneDefinition> by lazy {
-    JsonReader.readMilestones(readResource("milestones.json5")).toSetStrict()
+    readResources(MILESTONE_FILENAMES, JsonReader::readMilestones).toSetStrict()
   }
 
   override val colonyTileDefinitions: Set<ColonyTileDefinition> by lazy {
-    JsonReader.readColonyTiles(readResource("colonies.json5")).toSetStrict(::ColonyTileDefinition)
+    JsonReader.readColonyTiles(readResource("bundles/ColoniesExpansion/colonies.json5"))
+        .toSetStrict(::ColonyTileDefinition)
   }
 
   override val customClasses: Set<CustomClass> by ::canonCustomClasses
 
   private fun readResource(filename: String): String = CanonResources.read(filename)
+
+  private fun <T> readResources(
+      filenames: List<String>,
+      reader: (String) -> List<T>,
+  ): List<T> = filenames.flatMap { reader(readResource(it)) }
 
   public val SIMPLE_GAME = GameSetup(this, "BM", 2)
 }
