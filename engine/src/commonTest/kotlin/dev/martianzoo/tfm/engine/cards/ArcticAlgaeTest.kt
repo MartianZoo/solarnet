3b2e17b1f49853a0936da7b0a683f49accb03813
Kevin Bourrillion <kevinb9n@gmail.com>
2026-08-17 22:08:25 -0700

    Audit sequencing tests and decision rules

--- engine/src/commonTest/kotlin/dev/martianzoo/tfm/engine/cards/ArcticAlgaeTest.kt
+++ engine/src/commonTest/kotlin/dev/martianzoo/tfm/engine/cards/ArcticAlgaeTest.kt
@@ -1,6 +1,10 @@
 package dev.martianzoo.tfm.engine.cards
 
+import dev.martianzoo.api.Exceptions.TaskException
+import dev.martianzoo.engine.AutoExecMode.FIRST
+import dev.martianzoo.engine.AutoExecMode.NONE
 import dev.martianzoo.tfm.engine.cardnames.*
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import kotlin.test.Test
 
@@ -14,7 +18,7 @@ class ArcticAlgaeTest : CardTest() {
   }
 
   @Test
-  fun `after Arctic Algae grants plants, p1 plays Giant Ice Asteroid`() {
+  fun `Giant Ice Asteroid can resolve oceans and their Arctic Algae reactions before its attack`() {
     newGame()
     val p2 = requireP2()
     p2.manual("$ArcticAlgae, Plant")
@@ -22,11 +26,39 @@ class ArcticAlgaeTest : CardTest() {
     engine.phase("Action")
 
     p1.playProject(GiantIceAsteroid, 36) {
+      p1.autoExecMode = NONE
+      p2.autoExecMode = NONE
+      shouldThrow<TaskException> { p2.doTask("2 Plant") }
       doTask("OceanTile<Tharsis_1_2>")
+      p2.doTask("2 Plant")
       doTask("OceanTile<Tharsis_1_4>")
+      p2.doTask("2 Plant")
       doTask("-6 Plant<Player2>")
+      p1.autoExecMode = FIRST
+      p2.autoExecMode = FIRST
     }
 
     p2.count("Plant") shouldBe 0
+  }
+
+  @Test
+  fun `Giant Ice Asteroid can attack before its oceans and Arctic Algae reactions`() {
+    newGame()
+    val p2 = requireP2()
+    p2.manual("$ArcticAlgae, Plant")
+    p1.manual("ProjectCard, 36 Megacredit")
+    engine.phase("Action")
+
+    p1.playProject(GiantIceAsteroid, 36) {
+      p1.autoExecMode = NONE
+      p2.autoExecMode = NONE
+      doTask("-2 Plant<Player2>")
+      doTask("OceanTile<Tharsis_1_2>")
+      doTask("OceanTile<Tharsis_1_4>")
+      p1.autoExecMode = FIRST
+      p2.autoExecMode = FIRST
+    }
+
+    p2.count("Plant") shouldBe 4
   }
 }
---
+++ engine/src/commonTest/kotlin/dev/martianzoo/tfm/engine/cards/MiningAreaTest.kt
@@ -0,0 +1,46 @@
+package dev.martianzoo.tfm.engine.cards
+
+import dev.martianzoo.api.Exceptions.DependencyException
+import dev.martianzoo.api.Exceptions.NotNowException
+import dev.martianzoo.tfm.engine.cardnames.*
+import io.kotest.assertions.throwables.shouldThrow
+import kotlin.test.Test
+
+class MiningAreaTest : CardTest() {
+  @Test
+  fun `with a steel area adjacent, plays Mining Area`() {
+    newGame()
+    p1.manual("CityTile<Tharsis_2_1>")
+    p1.manual("$MiningArea") {
+          doTask("Tile064<Tharsis_1_1>")
+        }
+        .expect("2 Steel, PROD[Steel]")
+  }
+
+  @Test
+  fun `with a titanium area adjacent, plays Mining Area`() {
+    newGame()
+    p1.manual("CityTile<Tharsis_7_9>")
+    p1.manual("$MiningArea") {
+          doTask("Tile064<Tharsis_8_9>")
+        }
+        .expect("Titanium, PROD[Titanium]")
+  }
+
+  @Test
+  fun `without an adjacent tile, tries to play Mining Area`() {
+    newGame()
+    shouldThrow<DependencyException> {
+      p1.manual("$MiningArea") { doTask("Tile064<Tharsis_1_1>") }
+    }
+  }
+
+  @Test
+  fun `with a card-bonus area selected, tries to play Mining Area`() {
+    newGame()
+    p1.manual("CityTile<Tharsis_2_1>")
+    shouldThrow<NotNowException> {
+      p1.manual("$MiningArea") { doTask("Tile064<Tharsis_3_2>") }
+    }
+  }
+}
