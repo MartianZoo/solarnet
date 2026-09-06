cb02de4a3ae576fdc16a21a2418cf9a1d24d6da8
Kevin Bourrillion <kevinb9n@gmail.com>
2026-09-06 13:01:33 -0700

    Fix card location transitions

--- test/common/dev/martianzoo/tfm/tests/cards/SearchForLifeTest.kt
+++ test/common/dev/martianzoo/tfm/tests/cards/SearchForLifeTest.kt
@@ -6,6 +6,24 @@ import kotlin.test.Test
 
 internal class SearchForLifeTest : CardTest() {
   @Test
+  internal fun `Reveals a project card before checking its microbe tag`() {
+    newGame()
+    engine.phase("Action")
+    p1.manual("$SearchForLife, 1 MC")
+
+    p1.cardAction1(SearchForLife) {
+      p1.assertCounts(1 to "ProjectCard<Revealed>", 0 to "ProjectCard<Hand>")
+      doTask("Science<$SearchForLife>")
+    }
+
+    p1.assertCounts(
+        0 to "ProjectCard<Revealed>",
+        0 to "ProjectCard<Hand>",
+        1 to "Science<$SearchForLife>",
+    )
+  }
+
+  @Test
   internal fun `Scores three points when it has a science resource`() {
     newGame()
     engine.phase("Action")
